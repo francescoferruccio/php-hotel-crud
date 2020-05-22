9f70217ff33c7284d4cf985f092b5a3bd3db52ed
Francesco <francesco.ferruccio88@gmail.com>
2020-05-22 14:52:48 +0200

    esercizio completo

--- main.js
+++ main.js
@@ -3,9 +3,27 @@
 // - main.js [che fa ajax su getPagamenti.php]
 // - getPagamenti.php [che legge i dati da db come visto a lezione, attraverso il mysqli]
 // Naturalmente importanza 0 alla grafica, basta riuscire a stampare in pagina i dati in qualche modo.
+function getPagamenti() {
+  $.ajax({
+    url: 'getPagamenti.php',
+    method: 'GET',
+    success: function(data) {
+      var target = $('#target');
+      target.html('<h1>Pagamenti</h1>');
+
+      for (var pagamento of data) {
+        console.log(pagamento);
+        target.append('<li>' + pagamento.status + ' - ' + pagamento.price + '</li>');
+      }
+    },
+    error: function(err) {
+      console.error(err);
+    }
+  });
+}
 
 function init() {
-  console.log('hello world');
+  getPagamenti();
 }
 
 $(document).ready(init);
