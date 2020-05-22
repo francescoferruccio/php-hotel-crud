0ed34937a8b03f64bad8091b1f95529f89e3adb9
Francesco <francesco.ferruccio88@gmail.com>
2020-05-22 14:45:46 +0200

    api funzionante

---
+++ getPagamenti.php
@@ -0,0 +1,37 @@
+<?php
+  header('Content-Type: application/json');
+
+  $server = 'localhost';
+  $username = 'root';
+  $password = 'root';
+  $dbName = 'hoteldb';
+
+  $conn = new mysqli($server, $username, $password, $dbName);
+
+  if($conn -> connect_errno) {
+    echo $conn -> connect_errno;
+    return;
+  }
+
+  $sql = '
+    SELECT status, price
+    FROM pagamenti
+  ';
+
+  $result = $conn -> query($sql);
+
+  if($result -> num_rows < 1) {
+    echo "nessun risultato";
+    return;
+  }
+
+  $res = [];
+
+  while($row = $result -> fetch_assoc()) {
+    $res[] = $row;
+  }
+  $conn -> close();
+
+  echo json_encode($res);
+
+?>
---
+++ main.js
@@ -0,0 +1,11 @@
+// Nell'ottica di quanto visto a lezione, produrre una lista in HTML con stato e prezzo di tutti i pagamenti. I files saranno quindi 3:
+// - index.html [che importa il JS]
+// - main.js [che fa ajax su getPagamenti.php]
+// - getPagamenti.php [che legge i dati da db come visto a lezione, attraverso il mysqli]
+// Naturalmente importanza 0 alla grafica, basta riuscire a stampare in pagina i dati in qualche modo.
+
+function init() {
+  console.log('hello world');
+}
+
+$(document).ready(init);
